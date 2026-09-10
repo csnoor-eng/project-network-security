3428470e0ee0f6bb6bef1168bbb9b9bd7e28160f
csnoor-eng <narooalameri@gmail.com>
2026-09-10 13:05:16 +0300

    noor2

---
+++ lib/noor2.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class A extends StatelessWidget {
+  A({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Directionality(
+        textDirection: TextDirection.rtl,
+         child: Column(
+          children: [
+            Text("1"),
+            Row(
+              children: [
+                Text("noor"),
+                Text("najeeb"),
+                Text("alameri")
+              ],
+            )
+          ],
+         ),
+         ),
+    );
+  }
+}
